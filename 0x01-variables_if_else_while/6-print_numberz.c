f780ad51bf427615921c26effd9895c695789e8e
Morabet <ismailelmorabet2@gmail.com>
2023-06-16 11:12:06 -0700

    putchar prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  *
+  * Return: always (success)
+  */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
