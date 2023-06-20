75941b3cf3107a8b937ceaa5fcd58fd41e16d368
Morabet <ismailelmorabet2@gmail.com>
2023-06-20 11:43:21 -0700

    prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,15 +7,21 @@ void times_table(void)
 {
 	int i = 0;
 	int j = 0;
+	int k;
 
 	while (i < 10)
 	{
 		j = 0;
 		while (j < 10)
 		{
+			k = i * j;
+			if (k >= 0 && k <= 9)
+				_putchar(k + '0');
+			else
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			if (j != 9)
 			{
-				_putchar((i * j) + '0');
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
