527aeeb1b8e9632acadb108b3ef5b9fe5505d7cc
Morabet <ismailelmorabet2@gmail.com>
2023-06-20 15:24:34 -0700

    the first 50 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * main - entry point
+  *
+  *
+  */
+int main(void)
+{
+	int i = 3;
+	int f = 1, s = 2, next;
+
+	printf("%d\n", f);
+	printf("%d\n", s);
+	while (i <= 50)
+	{
+		next = f + s;
+		printf("%d\n", next);
+		f = s;
+		s = next;
+		i++;
+	}
+}
