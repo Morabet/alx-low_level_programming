8b9d59e30a6d49cc409a3b424ce68705212b6258
Morabet <ismailelmorabet2@gmail.com>
2023-06-20 05:02:35 -0700

    hat prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * main - entry point
+  *
+  * Return: always (success)
+  */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_alphabet - entry point
+  *
+  */
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		_putchar(i);
+		i++;
+	}
+	putchar('\n');
+}
