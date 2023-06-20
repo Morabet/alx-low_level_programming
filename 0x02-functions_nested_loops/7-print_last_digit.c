f5af210f5fa07b0cc96e601aa263a1803bb0f124
Morabet <ismailelmorabet2@gmail.com>
2023-06-20 06:55:13 -0700

    _last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,11 +5,12 @@
   *
   * Return: value of the last digits
   */
-int print_last_digit(int x)
+int print_last_digit(int n)
 {
+	int x = n % 10;
 	if (x < 0)
 		x = -x;
-	_putchar(x % 10 + '0');
+	_putchar(x + '0');
 
-	return (x % 10);
+	return (x);
 }
