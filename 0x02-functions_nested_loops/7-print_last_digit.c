bbf96581779dbcbbaf03938d87885add0b635bcd
Morabet <ismailelmorabet2@gmail.com>
2023-06-20 06:57:06 -0700

    _last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
   * print_last_digit - entry point
-  * @x: param
+  * @n: param
   *
   * Return: value of the last digits
   */
 int print_last_digit(int n)
 {
 	int x = n % 10;
+
 	if (x < 0)
 		x = -x;
 	_putchar(x + '0');
