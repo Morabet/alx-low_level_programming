83883612ea8bea06585d89808c98cbbe7d578de4
Morabet <ismailelmorabet2@gmail.com>
2023-06-20 15:14:07 -0700

    ist all the natural numbers below 10 that are multiples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,12 +13,9 @@ int main(void)
 	while (i <= 1024)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
-		{
 			sum += i;
-			printf("%d", sum);
-		}
 		i++;
 	}
-	printf("\n");
+	printf("%d\n", sum);
 	return (0);
 }
