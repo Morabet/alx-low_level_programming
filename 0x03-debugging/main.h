d2e349bb82d21fcba280884b4efa41586b6dae1e
Morabet <ismailelmorabet2@gmail.com>
2023-06-21 04:20:36 -0700

    Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,11 @@
 #ifndef HEADER
 #define HEADER
 #include <stdio.h>
+/**
+  * positive_or_negative - test id num is positive
+  * @i: param
+  *
+  * Return: int
+  */
 void positive_or_negative(int i);
 #endif
