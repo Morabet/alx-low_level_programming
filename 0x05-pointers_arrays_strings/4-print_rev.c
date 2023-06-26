09c6665ee9f9cf309ced6f249e1ee68bd7ddb1ed
Morabet <ismailelmorabet2@gmail.com>
2023-06-26 04:39:44 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+  * print_rev - entry point
+  * @s: param
+  *
+  */
+void print_rev(char *s)
+{
+	int i;
+	int size = strlen(s);
+
+	for (i = 0; i < size; i++)
+	{
+		*s[i] = *s[size - i];
+		_putchar(*s);
+	}
+	_putchar("\n");
+}
