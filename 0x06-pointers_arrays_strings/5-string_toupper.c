839d106cb82355b8c584d74bf54d8419adb2c1f4
Morabet <ismailelmorabet2@gmail.com>
2023-06-29 09:04:52 -0700

    string toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * string_toupper - entry point
+  * @s: param
+  *
+  * Return: char
+  */
+char *string_toupper(char *s)
+{
+	char *c;
+
+	c = s;
+
+	while (*c != '\0')
+	{
+		if(*c == '\\')
+			c++;
+		if (*c >= 'a' && *c <= 'z')
+		{
+			*c -= ' ';
+		}
+		c++;
+	}
+
+	return (s);
+}
