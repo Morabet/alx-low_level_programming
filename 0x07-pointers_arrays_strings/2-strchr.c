e0c5a74064561c0b0f1022fd540fb2b852b16c37
Morabet <ismailelmorabet2@gmail.com>
2023-07-03 03:32:03 -0700

    strchar

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,7 +9,7 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i = 0
 
 	while (s[i] != '\0')
 	{
@@ -19,5 +19,7 @@ char *_strchr(char *s, char c)
 		}
 		i++;
 	}
+	if (s[i] == c)
+		return ('\0');
 	return (NULL);
 }
