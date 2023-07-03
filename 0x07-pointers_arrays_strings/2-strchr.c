8e753647e8d1e455d103fa023c1ca3903bc14e36
Morabet <ismailelmorabet2@gmail.com>
2023-07-03 03:02:19 -0700

    strchar

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr -  find char in string.
+ * @s: pointer to the string.
+ * @c: char to find.
+ *
+ * Return: pointer to first c.
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return ('\0');
+}
