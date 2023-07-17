323530c408d0d07e5085b4cc24491256bebe4d52
Morabet <ismailelmorabet2@gmail.com>
2023-07-17 12:14:01 -0700

    pi

--- 0x0D-preprocessor/0-object_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MACRO_PI
-#define _MACRO_PI
-
-#define PI 3.14159265359
-
-#endif
--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MMAIN_H
+#ifndef _MACRO_PI
+#define _MACRO_PI
 
 #define PI 3.14159265359
 
