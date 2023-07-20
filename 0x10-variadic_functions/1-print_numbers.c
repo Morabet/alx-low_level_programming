0671d2ba256e49ed3ca3b92e0098145f96323130
Morabet <ismailelmorabet2@gmail.com>
2023-07-20 10:19:12 -0700

    task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,8 +16,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (separator == NULL)
 		separator = "";
 	va_start(ap, n);
-	for (i = 0; i < count - 1; i++)
-		printf("%d%s", va_arg(ap, int), sepparator);
+	for (i = 0; i < n - 1; i++)
+		printf("%d%s", va_arg(ap, int), separator);
 	printf("%d\n", va_arg(ap, int));
 	va_end(ap);
 
