cc7b7b94c41b0c47bf596e49788a893da7ccc376
Morabet <ismailelmorabet2@gmail.com>
2023-08-07 18:26:43 -0700

    3cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,37 +1,4 @@
 #include "main.h"
-
-/**
- * open_from - open the first file
- * @from: pointer to the FD
- * @name: file name
- * Return: void.
- */
-void open_from(int *from, char *name)
-{
-	*from = open(name, O_RDONLY);
-	if (*from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", name);
-		exit(98);
-	}
-}
-/**
- * open_to - open the second file
- * @to: pointer to the second FD
- * @from: pointer to the first FD
- * @name: file name
- * Return: void.
- */
-void open_to(int *to, int *from, char *name)
-{
-	*to = open(name, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (*to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", name);
-		close(*from);
-		exit(99);
-	}
-}
 /**
  * main - check the code for ALX School students.
  *
@@ -45,14 +12,15 @@ int main(int ac, char **av)
 	char txt[1024];
 
 	if (ac != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 
-	open_from(&from, av[1]);
+	from = open(av[1], O_RDONLY);
+	if (from == -1)
+		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	open_to(&to, &from, av[2]);
+	to = open(name, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to == -1)
+		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
 
 	while (r1 == 1024)
 	{
