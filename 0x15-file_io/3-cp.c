87daeff91b51344693f2382d3d8665c2d52620f6
Morabet <ismailelmorabet2@gmail.com>
2023-08-07 18:30:20 -0700

    3cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,7 +18,7 @@ int main(int ac, char **av)
 	if (from == -1)
 		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	to = open(name, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (to == -1)
 		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
 
