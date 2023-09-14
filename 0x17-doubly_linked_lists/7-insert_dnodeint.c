8cdd3f92a7de056f5d024962da5cda258bba68d7
Morabet <ismailelmorabet2@gmail.com>
2023-09-14 11:57:35 -0700

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	while (current)
 	{
 		current = current->next;
-		len++;
 	}
 	if (current->next == NULL)
 		return (add_dnodeint_end(h, n));
