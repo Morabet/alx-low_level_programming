68273197db09e7685b5d702ca023802d0e1e034d
Morabet <ismailelmorabet2@gmail.com>
2023-09-14 12:41:18 -0700

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 		len++;
 	}
-	if (len <= idx)
+	if (len == idx)
 		return (add_dnodeint_end(h, n));
 
 	node = malloc(sizeof(dlistint_t));
