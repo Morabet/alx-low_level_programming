66c616d2883256f82674789540527f2fc382138b
Morabet <ismailelmorabet2@gmail.com>
2023-11-22 10:43:38 -0800

    fic

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,13 +12,8 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-
-                if j > 0 or grid[i][j - 1] == 0:
+                if j > 0 and grid[i][j - 1] == 0:
                     p += 1
-                if j + 1 < len(grid[i]) or grid[i][j + 1] == 0:
+                if i > 0 and grid[i - 1][j] == 0:
                     p += 1
-                if i > 0 or grid[i - 1][j] == 0:
-                    p += 1
-                if i + 1 < len(grid) or grid[i + 1][j] == 0:
-                    p += 1
-    return p
+    return p * 2
