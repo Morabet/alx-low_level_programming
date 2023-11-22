f5111c97279b453dce1685bbc4c91a49aa72fbb1
Morabet <ismailelmorabet2@gmail.com>
2023-11-22 11:06:07 -0800

    fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,7 +8,6 @@ def island_perimeter(grid):
     to calculate the perimeter of the island
     """
 
-
     p = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
@@ -20,6 +19,6 @@ def island_perimeter(grid):
                     p += 1
                 if i > 0 and grid[i - 1][j] == 0:
                     p += 1
-                if i  + 1 < len(grid) and grid[i + 1][j] == 0:
+                if i + 1 < len(grid) and grid[i + 1][j] == 0:
                     p += 1
     return p
